548f00f8972756e03a5186a27ae88db0a454df3a
Brian <fredyfontaine@yahoo.co.uk>
2016-11-26 02:18:15 +0000

    added Angular so that info link boxes link to partials directly below them. Also changed the main photo

---
+++ scripts/app.js
@@ -0,0 +1,29 @@
+﻿
+(function (angular) {
+    'use strict';
+    /* See: https://github.com/johnpapa/angular-styleguide:
+        "Wrap Angular components in an Immediately Invoked Function Expression (IIFE).
+        Why?: An IIFE removes variables from the global scope. This helps prevent variables and function declarations from living longer than expected in the global scope, which also helps avoid variable collisions.
+        Why?: When your code is minified and bundled into a single file for deployment to a production server, you could have collisions of variables and many global variables. An IIFE protects you against both of these by providing variable scope for each file." */
+
+    angular.module("MainModule", ['router'])
+    /*
+    angular.module("MainModule", ['router', 'anchorScrollExample', 'expandAccordionSectionsModule', 'changeArrowsAccordionSectionsModule'])
+    
+    angular.module('anchorScrollExample', [])
+      .controller('ScrollController', ['$scope', '$location', '$anchorScroll',
+        function ($scope, $location, $anchorScroll) {
+            $scope.gotoBottom = function () {
+                // set the location.hash to the id of
+                // the element you wish to scroll to.
+                $location.hash('bottom');
+
+                // call $anchorScroll()
+                $anchorScroll();
+            };
+        }]);*/
+
+})(window.angular);
+
+
+
---
+++ scripts/routes.js
@@ -0,0 +1,18 @@
+﻿/// <reference path="../partials/testPartial.html" />
+angular.module('router', ['ngRoute'])
+.config(function ($routeProvider) { //Angular dependancy injection 
+    $routeProvider
+                .when('/rooms', { templateUrl: 'infoLinks/partials/rooms.html' })
+         .when('/bar', { templateUrl: 'infoLinks/partials/bar.html' })
+        .when('/location', { templateUrl: 'infoLinks/partials/location.html' })
+        .when('/nearby', { templateUrl: 'infoLinks/partials/nearby.html' })
+        .when('/test', { template: '<h3>Test Page</h3>' })
+
+    .otherwise({ templateUrl: 'infoLinks/partials/rooms.html' });
+    reloadOnSearch: false;
+
+});
+
+
+
+
