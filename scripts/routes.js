40400c78cb4a427819f2c099b060309e4c59a9dc
Brian <fredyfontaine@yahoo.co.uk>
2017-01-15 01:47:17 +0000

    create new layout with nav bar under logo, and homepage image put into a partial

--- scripts/routes.js
+++ scripts/routes.js
@@ -2,13 +2,14 @@
 angular.module('router', ['ngRoute'])
 .config(function ($routeProvider) { //Angular dependancy injection 
     $routeProvider
+         .when('/index', { templateUrl: 'infoLinks/partials/welcome.html' })
         .when('/rooms', { templateUrl: 'infoLinks/partials/rooms.html' })
         .when('/bar', { templateUrl: 'infoLinks/partials/bar.html' })
         .when('/location', { templateUrl: 'infoLinks/partials/location.html' })
         .when('/attractions', { templateUrl: 'infoLinks/partials/attractions.html' })
         .when('/test', { template: '<h3>Test Page</h3>' })
 
-        .otherwise({ templateUrl: 'infoLinks/partials/rooms.html' });
+        .otherwise({ templateUrl: 'infoLinks/partials/welcome.html' });
         reloadOnSearch: false;
 
 });
