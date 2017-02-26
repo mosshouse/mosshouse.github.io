e30dc6995f0cbe7609ad146b4424559431b88f1c
Brian <fredyfontaine@yahoo.co.uk>
2017-02-26 23:26:30 +0000

    Renaming of partial links for SEO purposes

--- scripts/routes.js
+++ scripts/routes.js
@@ -3,13 +3,19 @@ angular.module('router', ['ngRoute'])
 .config(function ($routeProvider) { //Angular dependancy injection 
     $routeProvider
          .when('/index', { templateUrl: 'infoLinks/partials/welcome.html' })
-        .when('/accomodation', { templateUrl: 'infoLinks/partials/accomodation.html' })
-        .when('/rooms', { templateUrl: 'infoLinks/partials/accomodation.html' })
-        .when('/rooms-carousel', { templateUrl: 'infoLinks/partials/rooms.html' })
-        .when('/bar', { templateUrl: 'infoLinks/partials/bar.html' })
+
+        .when('/accommodation', { templateUrl: 'infoLinks/partials/accommodation-bunclody.html' })
+        .when('/rooms', { templateUrl: 'infoLinks/partials/accommodation.html' })
+        .when('/rooms-carousel', { templateUrl: 'infoLinks/partials/accommodation-bunclody.html' })
+
+        .when('/bar', { templateUrl: 'infoLinks/partials/bar-and-music.html' })
+        .when('/bar-and-music', { templateUrl: 'infoLinks/partials/bar-and-music.html' })
+
         .when('/location', { templateUrl: 'infoLinks/partials/location.html' })
-        .when('/attractions', { templateUrl: 'infoLinks/partials/attractions.html' })
-        .when('/test', { template: '<h3>Test Page</h3>' })
+
+        .when('/bunclody-attractions', { templateUrl: 'infoLinks/partials/bunclody-attractions.html' })
+
+
 
         .otherwise({ templateUrl: 'infoLinks/partials/welcome.html' });
         reloadOnSearch: false;
